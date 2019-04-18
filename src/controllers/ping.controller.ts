8392f78fb242ae651b2c1df898c21708bc283930
Prateek Rastogi <prastogi34@gmail.com>
2019-04-18 20:26:44 +0530

    crafting ping as initiation hook

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,5 +1,7 @@
 import {Request, RestBindings, get, ResponseObject} from '@loopback/rest'
-import {inject} from '@loopback/context'
+import {inject, Context, BindingScope} from '@loopback/context'
+
+let count: number = 0
 
 /**
  * OpenAPI response for ping()
@@ -40,12 +42,19 @@ export class PingController {
     }
   })
   ping(): object {
+    count++
     // Reply with a greeting, the current time, the url, and request headers
     return {
-      greeting: `Hello from LoopBack`,
+      greeting: `Hello from LoopBack <count:${count}>`,
       date: new Date(),
       url: this.req.url,
       headers: Object.assign({}, this.req.headers)
     }
   }
 }
+
+const appCtx = new Context('application')
+appCtx
+  .bind('controllers.PingController')
+  .toClass(PingController)
+  .inScope(BindingScope.SINGLETON)
