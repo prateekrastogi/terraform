e681de8340c9c6a6c1f5676d0efc218b91aa0e95
Prateek Rastogi <prastogi34@gmail.com>
2019-04-18 19:45:18 +0530

    reverted ping controller to default state

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,7 +1,5 @@
 import {Request, RestBindings, get, ResponseObject} from '@loopback/rest'
-import {inject, Context, BindingScope} from '@loopback/context'
-
-let count: number = 0
+import {inject} from '@loopback/context'
 
 /**
  * OpenAPI response for ping()
@@ -42,19 +40,12 @@ export class PingController {
     }
   })
   ping(): object {
-    count++
     // Reply with a greeting, the current time, the url, and request headers
     return {
-      greeting: `Hello from LoopBack <count:${count}>`,
+      greeting: `Hello from LoopBack`,
       date: new Date(),
       url: this.req.url,
       headers: Object.assign({}, this.req.headers)
     }
   }
 }
-
-const appCtx = new Context('application')
-appCtx
-  .bind('controllers.PingController')
-  .toClass(PingController)
-  .inScope(BindingScope.SINGLETON)
