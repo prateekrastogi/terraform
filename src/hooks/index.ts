d6d8a722c9d9b5145d356aebfff5d057a93b6206
Prateek Rastogi <prastogi34@gmail.com>
2019-04-19 04:25:21 +0530

    need to switch to native shell for chromium emulation etc.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,7 @@ import {Observable, from} from 'rxjs'
 import {register} from './github'
 
 export function initiate(): Observable<number> {
+  register().subscribe()
+  
   return from([10, 20, 30])
 }
