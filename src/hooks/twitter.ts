27770ab96c2bd7e8229a876ca38e6a711a6a5896
Prateek Rastogi <prastogi34@gmail.com>
2019-05-03 03:54:43 +0530

    added cross-env dep

--- src/hooks/twitter.ts
+++ src/hooks/twitter.ts
@@ -3,14 +3,17 @@ import {Observable, from} from 'rxjs'
 import {concatMap, tap} from 'rxjs/operators'
 
 const TWITTER_URL = `https://twitter.com/`
+const LAUNCH_MODE = {headless: process.env.NODE_ENV === 'prod'}
+const VIEWPORT_SIZE = {height: 1280, width: 720}
 
 export function register(): Observable<boolean> {
-  const openWebPage = from(puppeteer.launch()).pipe(
+  const openWebPage = from(puppeteer.launch(LAUNCH_MODE)).pipe(
     concatMap(async browser => {
       const page = await browser.newPage()
+      await page.setViewport(VIEWPORT_SIZE)
       await page.goto(TWITTER_URL)
-      console.log(await page.content())
-
+      await page.content()
+      console.log(Math.random())
       return true
     })
   )
